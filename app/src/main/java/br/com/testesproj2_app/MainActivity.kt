79fd289810ea04837b4d669c2b3eb792d7bd7e3f
Roberto camelo <rj201190@gmail.com>
2021-06-09 17:14:45 -0300

    Update(Lista de regioes respondendo)
    Lista de estados(Norte e Nordeste prontos)

--- app/src/main/java/br/com/testesproj2_app/MainActivity.kt
+++ app/src/main/java/br/com/testesproj2_app/MainActivity.kt
@@ -13,21 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById<ListView>(R.id.listview_regioes)
+        val listView_regioes = findViewById<ListView>(R.id.listview_regioes)
         val listViewestados=findViewById<View>(R.id.listview_estados)
         val nomesRegioes = arrayOf("Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul")
         val estados= arrayOf("Tocantins, Pará, Amapá, Roraima, Amazonas, Acre, Rondônia ","Alagoas, Bahia, Ceará, Maranhão, Paraíba, Pernambuco, Piauí, Rio Grande do Norte, Sergipe","Goiás," +
                 "Mato Grosso, Mato Grosso do Sul, Distrito Federal","São Paulo, Rio de Janeiro, Minas Gerais, Espírito Santo", "Paraná, Santa Catarina, Rio Grande do Sul" )
 
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, nomesRegioes)
+            listView_regioes.adapter = adapter
 
-        val arrayAdapter : ArrayAdapter<String> = ArrayAdapter(
-            this, android.R.layout.simple_list_item_1, nomesRegioes)
 
-        listView.adapter = arrayAdapter
 
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            val intent =Intent(this@MainActivity, MainActivity2::class.java)
-            intent.putExtra("dados_estados", estados)
+        listView_regioes.setOnItemClickListener { parent, view, i, id ->
+            var item_clicado=parent.getItemAtPosition(i)
+            val intent =Intent(this, MainActivity2::class.java)
+            intent.putExtra("estados", item_clicado.toString())
+            //intent.putExtra("dados_estados", estados)
             Toast.makeText(this, "Item selecionado" + nomesRegioes[i], Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
--- app/src/main/java/br/com/testesproj2_app/MainActivity2.kt
+++ app/src/main/java/br/com/testesproj2_app/MainActivity2.kt
@@ -3,16 +3,50 @@ package br.com.testesproj2_app
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+import android.content.Intent
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val visualizar =findViewById<View>(R.id.listview_estados);
-        val TempHolder = intent.getStringExtra("dados_regiao")
+        var lista_estados=intent.getStringExtra("estados")
+        val lista_estatal=findViewById<ListView>(R.id.listview_estados2)
+
+
+
+        if (lista_estados.toString()=="Norte"){
+
+            val Nestados = arrayOf ("Tocantins", "Pará", "Amapá", "Roraima", "Amazonas", "Acre", "Rondônia")
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, Nestados)
+            lista_estatal.adapter = adapter
+
+        }else if (lista_estados.toString()=="Nordeste"){
+
+            val Nestados = arrayOf ("Alagoas", "Bahia", "Ceará", "Maranhão", "Paraíba", "Pernambuco", "Piauí", "Rio Grande do Norte", "Sergipe")
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, Nestados)
+            lista_estatal.adapter = adapter
+        }else if (lista_estados.toString()=="Centro-Oeste"){
+
+        }else if (lista_estados.toString()=="Sudeste"){
+
+        }else if (lista_estados.toString()=="Sul"){
+
+        }
+
+        lista_estatal.setOnItemClickListener { parent, view, i, id ->
+            Toast.makeText(this, "Item selecionado" + lista_estatal[i], Toast.LENGTH_LONG).show()
+        }
+
+
+       /* val visualizar =findViewById<View>(R.id.listview_estados);
+        val TempHolder = intent.getStringExtra("dados_regiao")*/
 
 
 
